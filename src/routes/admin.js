737e1837c3ccf8b0d72a0890cdcec2897621b4f8
nicojoaquin <nicojoaquin1998@gmail.com>
2022-02-15 05:39:56 -0300

    inputs

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -14,9 +14,9 @@ const router = Router();
 
 //POST
 router.post('/',
-  check('nombre', "El nombre es obligatorio").not().isEmpty(),
-  check('comp', "La composición es obligatoria").not().isEmpty(),
-  check('desc', "La descripción es obligatoria").not().isEmpty().isLength({min: 50}).withMessage('La descripción debe contener mas de 50 caracteres'),
+  check('nombre', "El nombre es obligatorio").trim().not().isEmpty(),
+  check('comp', "La composición es obligatoria").trim().not().isEmpty(),
+  check('desc', "La descripción es obligatoria").trim().not().isEmpty().isLength({min: 50}).withMessage('La descripción debe contener mas de 50 caracteres'),
   inputsValidator,
   createProduct
 );
@@ -29,9 +29,9 @@ router.get('/tela/:id', readProductById);
 
 // //PUT
 router.put('/:id',
-  check('nombre', "El nombre no puede estar vacio").not().isEmpty(),
-  check('comp', "La composición no puede estar vacia").not().isEmpty(),
-  check('desc', "La descripción no puede estar vacia").not().isEmpty().isLength({min: 50}).withMessage('La descripción debe contener mas de 50 caracteres'),
+  check('nombre', "El nombre no puede estar vacio").trim().not().isEmpty(),
+  check('comp', "La composición no puede estar vacia").trim().not().isEmpty(),
+  check('desc', "La descripción no puede estar vacia").trim().not().isEmpty().isLength({min: 50}).withMessage('La descripción debe contener mas de 50 caracteres'),
   inputsValidator,
   updateProduct
 );
